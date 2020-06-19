5e47b35a2f833ab77597ab4190be71959f6c0f6b
Victor Petsev <Vpetsev96@gmail.com>
2020-06-19 15:33:06 -0500

    moved business object to props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Business from "../Business/Business"
 import BusinessList from "../BusinessList/BusinessList"
 import SearchBar from "../SearchBar/SearchBar"
 
-function App() {
-  return (
-    <div>
-      <h1>ravenous</h1>
-      <SearchBar />
-      <BusinessList />
-    </div>
-  );
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
+const businesses = [business, business, business, business, business, business]
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList businesses={businesses}/>
+      </div>
+    )
+  }
 }
 
 export default App;
